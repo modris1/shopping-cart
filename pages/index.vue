1535712f8c24dfb96ad6d4ec199231dc69c7d14b
Modris Upenieks <modris.upen@gmail.com>
2025-02-01 18:55:33 +0200

    Project setup

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <Title>Home</Title>
+    <div class="text-4xl text-bold text-center">Shopping cart</div>
+</template>
+
+<script setup>
+    const cartStore = useCartStore();
+    console.log(cartStore.items);
+</script>
